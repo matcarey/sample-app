a4d106f2f7d634da0ed5c0af6777c43b10ad6669
Mat Carey <mat@dorightdigital.com>
2015-03-09 23:46:20 +0000

    Basic demo.

---
+++ server.js
@@ -0,0 +1,34 @@
+var io = require('./lib/io');
+var remaining = 5;
+var right = 0;
+var wrong = 0;
+
+io.divider();
+io.output('Try some basic maths');
+io.divider();
+function askQuestion() {
+  var a = Math.ceil(Math.random()*50);
+  var b = Math.ceil(Math.random()*50);
+  io.input('What is ' + a + ' + ' + b + '?').then(function (answer) {
+    if (answer === ('' + (a + b))) {
+      right++;
+    } else {
+      wrong++;
+    }
+    askQuestionOrDisplayResults();
+  });
+}
+function askQuestionOrDisplayResults() {
+  if (remaining > 0) {
+    remaining--;
+    askQuestion();
+  } else {
+    io.divider();
+    io.output('Results');
+    io.divider();
+    io.output('Right:', right);
+    io.output('Wrong:', wrong);
+    io.end();
+  }
+}
+askQuestionOrDisplayResults();
